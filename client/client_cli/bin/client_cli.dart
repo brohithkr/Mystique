a7716688b9cf7c18311b8916b299f3f4216113c5
brohithkr <brohithkr@gmail.com>
2023-10-21 22:29:18 +0530

    half baked cli chat app

---
+++ client/client_cli/bin/client_cli.dart
@@ -0,0 +1,47 @@
+// import 'dart:ffi';
+import 'package:process_run/shell.dart';
+
+import 'package:web_socket_client/web_socket_client.dart';
+import 'dart:io';
+
+//┌├┐└┘
+List<String> chatHistory = [];
+
+void displayRefresh(List<String> data) async {
+  var horizonalLine = "${"─" * 80}\n";
+  var res = horizonalLine;
+  // res += ;
+  for (var i in data) {
+    res += "$i\n";
+  }
+  res += horizonalLine;
+  res += "Type your message: ";
+  print(Process.runSync("clear", [], runInShell: true).stdout);
+  stdout.write(res);
+}
+
+void main() async {
+  // displayRefresh();
+  String username = stdin.readLineSync() ?? "user";
+  final socket =
+      WebSocket(Uri.parse('ws://localhost:3000/connect?username=$username'));
+
+  // Wait until a connection has been established.
+  await socket.connection.firstWhere((state) {
+    // print(state);
+    return state is Connected;
+  });
+
+  socket.messages.listen((message) {
+    // stdout.write("Type your message: ");
+    chatHistory.add(message);
+    displayRefresh(chatHistory);
+    String toSend = stdin.readLineSync() ?? "";
+    socket.send(toSend);
+    chatHistory.add(toSend);
+    displayRefresh(chatHistory);
+  });
+
+  // Send a message to the server.
+  // socket.send('ping');
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,13 +10,13 @@ var active_uids = new Map<string, User>() // get user data from uid of user
 var active_channels = new Map<string, string[]>()
 var waiting_uids: string[] = [];
 
-function generate_uid() : string {
+function generate_uid(): string {
     let rand_num = Math.random()
     let uid = Math.floor(rand_num * 1e6)
     return uid.toString()
 }
 
-function generate_unique_uid<V>(generate_uids: Map<string, V> | Set<string>) : string {
+function generate_unique_uid<V>(generate_uids: Map<string, V> | Set<string>): string {
     var uid = generate_uid()
     while (generate_uids.has(uid)) {
         uid = generate_uid()
@@ -28,19 +28,21 @@ let app = Bun.serve<User>({
     async fetch(req, server) {
         let url = new URL(req.url)
         let path = url.pathname
-        switch(path){
+        // console.log(path)
+        // console.log(url.searchParams)
+        switch (path) {
             case "/connect":
-                console.log(path)
-                let body = (await req.json())
-                let username = body.username
-                let uid = "u"+generate_unique_uid(active_uids)
+                // console.log(path)
+                let body = url.searchParams
+                let username = body.get("username") ?? "user"
+                let uid = "u" + generate_unique_uid(active_uids)
                 let user_data: User;
                 // active_uids.set("u" + uid, username)
                 var channel_id = "";
-                // console.log(waiting_uids)
-                if(waiting_uids.length == 0) {
+                console.log(waiting_uids)
+                if (waiting_uids.length == 0) {
                     waiting_uids.unshift(uid)
-                    channel_id = "c"+generate_unique_uid(active_channels)
+                    channel_id = "c" + generate_unique_uid(active_channels)
                     active_channels.set(channel_id, [uid])
                     user_data = {
                         username: username,
@@ -49,8 +51,8 @@ let app = Bun.serve<User>({
                     }
                 }
                 else {
-                    let paired_uid =  waiting_uids.pop()??""
-                    // console.log(paired_uid, active_uids)
+                    let paired_uid = waiting_uids.pop() ?? ""
+                    console.log(paired_uid, active_uids)
                     let paired_one = active_uids.get(paired_uid) as User
                     channel_id = paired_one.channel
                     active_channels.get(channel_id)?.push(uid)
@@ -61,12 +63,12 @@ let app = Bun.serve<User>({
                     }
                 }
                 active_uids.set(uid, user_data)
-                console.log(waiting_uids)
-                console.log(active_channels)
+                // console.log(waiting_uids)
+                // console.log(active_channels)
                 server.upgrade(
                     req, {
-                        data: user_data
-                    }
+                    data: user_data
+                }
                 )
                 return new Response(JSON.stringify(Object.fromEntries(active_uids)))
                 break
@@ -77,13 +79,13 @@ let app = Bun.serve<User>({
         open(ws) {
             let channel_id = ws.data.channel
             ws.subscribe(channel_id)
-            ws.publish(channel_id, `${ws.data.username} as entered the chat.`)
+            ws.publish(channel_id, `!->${ws.data.username} as entered the chat.`)
         },
         message(ws, message) {
             ws.publish(ws.data.channel, `${ws.data.username}: ${message}`)
         },
         close(ws) {
-            ws.publish(ws.data.channel, `${ws.data.username} has left the chat.`)
+            ws.publish(ws.data.channel, `!->${ws.data.username} has left the chat.`)
         }
     }
 })
