3eb449fea3b9a3c7d551b9cf5a470639a11f4b83
brohithkr <brohithkr@gmail.com>
2023-10-24 08:12:23 +0530

    server fixes and added navigation [deploy]

--- client/mystique_gui/lib/main.dart
+++ client/mystique_gui/lib/main.dart
@@ -42,15 +42,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TitleLogo(
-            context: context,
-          ),
-          SizedBox(height: 100),
-          InputForm(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TitleLogo(
+              context: context,
+            ),
+            SizedBox(height: 100),
+            InputForm(),
+          ],
+        ),
       ),
     );
   }
